f89d12edb004f30c3adbf133dc851c08ae16abd1
José Montero <josegabriel2549@gmail.com>
2017-03-05 17:50:48 -0500

    Comino Inicial Pokémon Go

---
+++ My Pokémon Go/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Pokémon Go
+//
+//  Created by José Montero on 5/3/17.
+//  Copyright © 2017 José Montero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
